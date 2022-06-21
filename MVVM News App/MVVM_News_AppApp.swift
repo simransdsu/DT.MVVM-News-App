2712e07e15a5a1584adff9acb7ca6fa56856ec06
Simran Preet Singh <simran5590@gmail.com>
2022-06-20 21:22:41 -0700

    Initial Commit

---
+++ MVVM News App/MVVM_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_News_AppApp.swift
+//  MVVM News App
+//
+//  Created by Simran Preet Narang on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
